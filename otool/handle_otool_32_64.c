b3921b5d8f6e871a82ef8431864ba5d7dec17786
nidzik <nicolas.idzik@yahoo.fr>
2017-03-29 11:54:10 +0200

    otool

---
+++ otool/handle_otool_32_64.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_otool_32_64.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwalle <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 12:10:23 by jwalle            #+#    #+#             */
+/*   Updated: 2016/11/26 12:10:25 by jwalle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_otool.h"
+
+static int	find_sector_and_segment_32(char *ptr, struct load_command *lc,
+	t_otool_env *e, int nsect)
+{
+	struct segment_command	*sg;
+	struct section			*s;
+	uint32_t				j;
+
+	sg = (struct segment_command *)lc;
+	s = (struct section *)((char *)sg + sizeof(struct segment_command));
+	j = 0;
+	while (j < sg->nsects)
+	{
+		if (!ft_strcmp((s + j)->sectname, SECT_TEXT) &&
+			!ft_strcmp((s + j)->segname, SEG_TEXT))
+		{
+			print_otool(s->size, s->addr, ptr + s->offset, e);
+			return (0);
+		}
+		nsect++;
+		j++;
+	}
+	return (nsect);
+}
+
+void		handle_otool_32(char *ptr, t_otool_env *e)
+{
+	int						nsect;
+	struct mach_header		*header;
+	struct load_command		*lc;
+
+	nsect = 1;
+	header = (struct mach_header*)ptr;
+	lc = (void *)ptr + sizeof(*header);
+	if (!e->cpu)
+		e->cpu = 32;
+	if (header->filetype == MH_DYLIB || header->filetype == MH_OBJECT)
+		e->lib = 1;
+	if (header->filetype == MH_BUNDLE)
+		e->bundle = 1;
+	if (header->filetype == MH_DYLINKER)
+		e->dylink = 1;
+	while (header->ncmds--)
+	{
+		if (lc->cmd == LC_SEGMENT)
+			nsect = find_sector_and_segment_32(ptr, lc, e, nsect);
+		lc = (void *)lc + lc->cmdsize;
+	}
+}
+
+static int	find_sector_and_segment_64(char *ptr, struct load_command *lc,
+	t_otool_env *e, int nsect)
+{
+	struct segment_command_64	*sg;
+	struct section_64			*s;
+	uint32_t					j;
+
+	j = 0;
+	sg = (struct segment_command_64 *)lc;
+	s = (struct section_64 *)((char *)sg + sizeof(struct segment_command_64));
+	while (j < sg->nsects)
+	{
+		if (!ft_strcmp((s + j)->sectname, SECT_TEXT) &&
+			!ft_strcmp((s + j)->segname, SEG_TEXT))
+		{
+			print_otool(s->size, s->addr, ptr + s->offset, e);
+			return (0);
+		}
+		nsect++;
+		j++;
+	}
+	return (nsect);
+}
+
+void		handle_otool_64(char *ptr, t_otool_env *e)
+{
+	int						nsect;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+
+	nsect = 1;
+	header = (struct mach_header_64*)ptr;
+	lc = (void *)ptr + sizeof(*header);
+	if (!e->cpu)
+		e->cpu = 64;
+	if (header->filetype == MH_DYLIB)
+		e->dylib = 1;
+	if (header->filetype == MH_BUNDLE)
+		e->bundle = 1;
+	if (header->filetype == MH_OBJECT)
+		e->obj = 1;
+	if (header->filetype == MH_DYLINKER)
+		e->dylink = 1;
+	//	printf("cpu %d\tdylib %d\tbundle %d\tobj %d\tdylink %d",e->cpu, e->dylib, e->bundle, e->obj, e->dylink);
+	while (header->ncmds--)
+	{
+		if (lc->cmd == LC_SEGMENT_64)
+			nsect = find_sector_and_segment_64(ptr, lc, e, nsect);
+		lc = (void *)lc + lc->cmdsize;
+	}
+}
---
+++ otool/main.c
@@ -0,0 +1,106 @@
+#include "ft_otool.h"
+
+static	int	g_magic_number[] = { MH_MAGIC, MH_MAGIC_64, MH_RANLIB,
+	FAT_CIGAM, FAT_MAGIC, 0};
+
+void	(*g_magic_functions[])(char*, t_otool_env*) =
+{handle_otool_32, handle_otool_64, handle_otool_library,
+	handle_otool_taf, handle_otool_fat};
+
+void		ft_otool(char *ptr, t_otool_env *e)
+{
+	int				i;
+	int				flag;
+	int				magic_num;
+
+	i = 0;
+	flag = 0;
+	magic_num = *(int *)ptr;
+	while (g_magic_number[i])
+	{
+		if (magic_num == g_magic_number[i])
+		{
+			g_magic_functions[i](ptr, e);
+			flag++;
+		}
+		i++;
+	}
+}
+
+int			process_file(int fd, t_otool_env *e)
+{
+	struct stat		buf;
+	char			*ptr;
+
+	if ((fstat(fd, &buf)) < 0)
+	{
+		ft_putstr("fstat error.\n");
+		return (EXIT_FAILURE);
+	}
+	if ((ptr = mmap(0, buf.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0))
+		== MAP_FAILED)
+	{
+		ft_putstr("mmap error.\n");
+		return (EXIT_FAILURE);
+	}
+	ft_otool(ptr, e);
+	if (munmap(ptr, buf.st_size) < 0)
+	{
+		ft_putstr("munmap error.\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+t_otool_env	*init_env(char *file)
+{
+	t_otool_env *e;
+
+	e = (t_otool_env *)malloc(sizeof(t_otool_env));
+	e->cpu = 0;
+	e->fat = 0;
+	e->lib = 0;
+	e->obj = 0;
+	e->bundle = 0;
+	e->dylink = 0;
+	e->dylib = 0;
+	e->file = ft_strdup(file);
+	return (e);
+}
+
+int			test_open(char *av)
+{
+	int			fd;
+	t_otool_env	*e;
+
+	e = NULL;
+	if ((fd = open(av, O_RDONLY)) < 0)
+	{
+		printf("%s : No such file or directory.\n", av);
+		return (EXIT_FAILURE);
+	}
+	e = init_env(av);
+	process_file(fd, e);
+	free(e->file);
+	free(e);
+	return (fd);
+}
+
+int			main(int ac, char **av)
+{
+	int	fd;
+	int	i;
+
+	fd = 0;
+	i = 1;
+	if (ac == 1)
+		ft_putstr("Usage: ft_otool <object file> ...\n");
+	else if (ac == 2)
+		test_open(av[1]);
+	else if (ac > 2)
+	{
+		while (i < ac)
+			test_open(av[i++]);
+	}
+	return (0);
+}
